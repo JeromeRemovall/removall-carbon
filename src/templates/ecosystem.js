f47d8f165a675008dca19a573d00d41bba723d0e
julien-wagentrutz <julien.wagentrutz@hetic.net>
2023-12-20 16:32:54 +0100

    [ecosystem] add verbatims on page

--- src/templates/ecosystem.js
+++ src/templates/ecosystem.js
@@ -9,6 +9,13 @@ import { graphql, useStaticQuery } from "gatsby";
 
 import { Helmet } from "react-helmet"
 
+// import Swiper and modules styles
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import isMobile from '../utils/global'
+
 const query = graphql`
 	query{
 		blocClients : allWpPost(
@@ -71,6 +78,42 @@ const query = graphql`
 				}
 			}
 		}
+		verbatimsFR : allWpPost(
+			filter: {categories: {nodes: {elemMatch: {slug: {eq: "verbatims"}}}}}
+			sort: {fields: verbatims___ordre, order: ASC}
+			) {
+			edges {
+				node {
+					verbatims {
+						fonctionDeLauteur
+						nomDeLauteur
+						ordre
+						verbatim
+						photoDeLauteur {
+							mediaItemUrl
+						}
+					}
+				}
+			}
+		}
+		verbatimsEN : allWpPost(
+			filter: {categories: {nodes: {elemMatch: {slug: {eq: "verbatims-en"}}}}}
+		sort: {fields: verbatims___ordre, order: ASC}
+			) {
+			edges {
+				node {
+					verbatims {
+						fonctionDeLauteur
+						nomDeLauteur
+						ordre
+						verbatim
+						photoDeLauteur {
+							mediaItemUrl
+						}
+					}
+				}
+			}
+		}
 	}
 `
 
@@ -79,24 +122,33 @@ function Ecosystem({ pageContext }){
 	const dataE = dataEcosystem.ecosysteme;
 
 	const data = useStaticQuery(query);
+	const [dataVerbatims, setDataVerbatims] = useState("");
 	const [dataCustomers, setDataCustomers] = useState("");
 	const [dataPartners, setDataPartners] = useState("");
 	const [metaLang, setMetaLang] = useState("");
+	const [space, setSpace] = useState(0);
+
 
 	useEffect(() => {
 		function getLanguage(){
 			if(window.location.href.match("/fr$") || window.location.href.match("/fr/")){
 				setDataCustomers(data.blocClients.nodes);
 				setDataPartners(data.blocPartenaires.edges);
+				setDataVerbatims(data.verbatimsFR.edges);
 				setMetaLang("fr");
 				console.log(data.blocPartenaires.edges)
 			}else if(window.location.href.match("/en$") || window.location.href.match("/en/")){
 				setDataCustomers(data.blocCustomers.nodes);
 				setDataPartners(data.blocPartners.edges);
+				setDataVerbatims(data.verbatimsEN.edges);
 				setMetaLang("en");
 			}
 		}
 		getLanguage();
+		if(!isMobile()){
+			setSpace(-(window.innerWidth/2.8))
+		}
+		
 	}, [data.blocClients.nodes, data.blocCustomers.nodes, data.blocPartenaires.edges, data.blocPartners.edges])
 
 	return(
@@ -144,6 +196,32 @@ function Ecosystem({ pageContext }){
 							</div>
 						</section>
 					:null }
+					{dataVerbatims.length > 0 ?
+					 <Swiper
+						modules={[Navigation, Pagination, Autoplay]}
+						spaceBetween={space}
+						slidesPerView={1}
+						pagination={{ clickable: true }}
+						className='swiper_verbatims'
+						autoplay={{
+							delay: 2500,
+							pauseOnMouseEnter: true,
+						}}
+					>
+						{dataVerbatims.map((verbatim, index)=> (
+							<SwiperSlide>
+								<div className='content_slide'>
+									<p>"{verbatim.node.verbatims.verbatim}"</p>
+									<div className='slide_info'>
+										<img src={verbatim.node.verbatims.photoDeLauteur.mediaItemUrl} alt=""/>
+										<p className='auteur'>{verbatim.node.verbatims.nomDeLauteur}</p>
+										<p className='fonction'>{verbatim.node.verbatims.fonctionDeLauteur}</p>
+									</div>
+								</div>
+							</SwiperSlide>
+						))}
+    				</Swiper>
+					:null}
 				</main>
 			: <Loader /> }
 		</Layout>
