6d8ba54c2910f359138be1f234a7d27614ddd4f8
julien-wagentrutz <julien.wagentrutz@hetic.net>
2025-01-17 23:05:51 +0100

    :bug: fix some style's issue

--- src/components/ressources/blocArticle/dist/icons.js
+++ src/components/ressources/blocArticle/dist/icons.js
@@ -6,11 +6,13 @@ var Icons = function (_a) {
     var data = _a.data;
     var renderItem = function (data, index) {
         var _a, _b;
-        return (react_1["default"].createElement(react_1["default"].Fragment, null, data["item" + index] && (react_1["default"].createElement("div", { className: 'icons' },
-            react_1["default"].createElement("div", { className: 'index' },
+        return (react_1["default"].createElement(react_1["default"].Fragment, null, data["item" + index] && (react_1["default"].createElement("div", { className: "icons" },
+            react_1["default"].createElement("div", { className: "index" },
                 react_1["default"].createElement("img", { src: (_a = data["icon" + index]) === null || _a === void 0 ? void 0 : _a.sourceUrl, alt: (_b = data["icon" + index]) === null || _b === void 0 ? void 0 : _b.altText })),
-            react_1["default"].createElement("div", { className: 'content' },
-                react_1["default"].createElement("div", { className: 'description', dangerouslySetInnerHTML: { __html: data["item" + index] } }))))));
+            react_1["default"].createElement("div", { className: "content" },
+                react_1["default"].createElement("div", { className: "description", dangerouslySetInnerHTML: {
+                        __html: data["item" + index]
+                    } }))))));
     };
     var renderImages = function () {
         var blocks = [];
@@ -19,6 +21,6 @@ var Icons = function (_a) {
         }
         return blocks;
     };
-    return (react_1["default"].createElement("div", { className: 'icons-container' }, renderImages()));
+    return (react_1["default"].createElement("div", { className: "icons-container" }, renderImages()));
 };
 exports["default"] = Icons;
--- src/components/ressources/blocArticle/icons.tsx
+++ src/components/ressources/blocArticle/icons.tsx
@@ -1,43 +1,57 @@
-import React from 'react';
+import React from "react";
 
-import '../../../scss/components/ressources/blocArticle/bloc.scss';
+import "../../../scss/components/ressources/blocArticle/bloc.scss";
 
 interface Props {
-	data: any
+  data: any;
 }
 
-const Icons: React.FC<Props> = ({data}) => {
+const Icons: React.FC<Props> = ({ data }) => {
+  const renderItem = (
+    data: any,
+    index: number
+  ) => {
+    return (
+      <>
+        {data[`item${index}`] && (
+          <div className="icons">
+            <div className="index">
+              <img
+                src={
+                  data[`icon${index}`]?.sourceUrl
+                }
+                alt={
+                  data[`icon${index}`]?.altText
+                }
+              />
+            </div>
+            <div className="content">
+              <div
+                className="description"
+                dangerouslySetInnerHTML={{
+                  __html: data[`item${index}`],
+                }}
+              ></div>
+            </div>
+          </div>
+        )}
+      </>
+    );
+  };
 
-	const renderItem = (data: any,index: number) => {
-		return (
-			<>
-				{data[`item${index}`] && (
-					<div className='icons'>
-						<div className='index'>
-							<img src={data[`icon${index}`]?.sourceUrl} alt={data[`icon${index}`]?.altText} />
-						</div>
-						<div className='content'>
-							<div className='description' dangerouslySetInnerHTML={{ __html: data[`item${index}`] }}></div>
-						</div>
-					</div>
-				)}
-			</>
-		);
-	}
+  const renderImages = () => {
+    const blocks = [];
+    for (let i = 1; i < 5; i++) {
+      blocks.push(renderItem(data, i));
+    }
+    return blocks;
+  };
 
-	const renderImages = () => {
-		const blocks = [];
-		for (let i = 1; i < 5; i++) {
-			blocks.push(renderItem(data, i));
-		}
-		return blocks;
-	}
-
-	return (
-		<div className='icons-container'>
-			{renderImages()}
-		</div>
-	);
+  return (
+    <div className="icons-container">
+      {renderImages()}
+    </div>
+  );
 };
 
 export default Icons;
